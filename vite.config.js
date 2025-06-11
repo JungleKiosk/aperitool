f0801bcd9c7cc999f315976bc9883b4128305c79
Fra <fra24eso@gmail.com>
2025-06-11 12:13:09 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Base path for the application, useful for deployment in subdirectories
   base: '/aperitool/',
   plugins: [vue()],
 })
