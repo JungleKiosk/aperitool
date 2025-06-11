58c339959a725008961bdbf886ed0c2ad5403494
Fra <fra24eso@gmail.com>
2025-06-11 12:31:58 +0200

    test push

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Base path for the application, useful for deployment in subdirectories
   base: '/aperitool/',
   plugins: [vue()],
 })
